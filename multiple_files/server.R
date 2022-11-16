4ab030407e5918ff8d46793f07ca04948d53d59c
dgunadharma20 <dgunadharma20@gmail.com>
2022-11-16 11:46:05 -0800

    multiple file shiny app demo completed

---
+++ multiple_files/server.R
@@ -0,0 +1,46 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+
+data <- read.csv("https://countlove.org/data/data.csv")
+
+data <- data %>% 
+  mutate(states = str_sub(Location, -2, -1)) %>% 
+  head(500)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  output$selectState <- renderUI({
+    selectInput("state", "Choose a state:", choices = unique(data$states))
+  })
+  
+  scatterPlot <- reactive({
+    plotData <- data %>% 
+      filter(states %in% input$state)
+    
+    ggplot(plotData, aes(x = Date, y = Attendees)) +
+      geom_point(aes(color = Location)) +
+      labs(x = "date of protests",
+           y = "number of attendees", 
+           title = paste("date vs attendees in", input$state))
+  })
+  
+  output$statePlot <- renderPlot({
+    scatterPlot()
+  })
+  
+  output$sampleText <- renderText({
+    paste("Hello! Welcome to my sample Shiny web app. This visualization shows a scatter of the number of attendees, depending
+            on the date of the protest and the state it happened in.")
+  })
+
+})
